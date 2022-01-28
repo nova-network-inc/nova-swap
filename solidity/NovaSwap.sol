8ef6945f055b992365f72213e0f5f8a3e15b57f7
SuperDev321 <superman.dev2020@mail.ru>
2022-01-28 05:11:51 +0200

    Add fee part

--- solidity/NovaSwap.sol
+++ solidity/NovaSwap.sol
@@ -2,7 +2,45 @@
 // NovaSwap™, by Nova Network Inc.
 // https://www.novanetwork.io/
 
-pragma solidity ^0.7.0;
+pragma solidity ^0.8.0;
+
+import './ReentrancyGuard.sol';
+
+
+// transfer helper library
+library TransferHelper {
+    function safeApprove(address token, address to, uint value) internal {
+        // bytes4(keccak256(bytes('approve(address,uint256)')));
+        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0x095ea7b3, to, value));
+        require(success && (data.length == 0 || abi.decode(data, (bool))), 'TransferHelper: APPROVE_FAILED');
+    }
+
+    function safeTransfer(address token, address to, uint value) internal {
+        // bytes4(keccak256(bytes('transfer(address,uint256)')));
+        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0xa9059cbb, to, value));
+        require(success && (data.length == 0 || abi.decode(data, (bool))), 'TransferHelper: TRANSFER_FAILED');
+    }
+
+    function safeTransferFrom(address token, address from, address to, uint value) internal {
+        // bytes4(keccak256(bytes('transferFrom(address,address,uint256)')));
+        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0x23b872dd, from, to, value));
+        require(success && (data.length == 0 || abi.decode(data, (bool))), 'TransferHelper: TRANSFER_FROM_FAILED');
+    }
+
+    function safeTransferETH(address to, uint value) internal {
+        (bool success,) = to.call{value:value}(new bytes(0));
+        require(success, 'TransferHelper: ETH_TRANSFER_FAILED');
+    }
+}
+
+
+// WETH interface
+interface IWETH {
+    function deposit() external payable;
+    function transfer(address to, uint value) external returns (bool);
+    function withdraw(uint) external;
+}
+
 
 // ERC-20 Interface
 interface IERC20 {
@@ -19,6 +57,7 @@ interface IERC20 {
     event Transfer(address indexed from, address indexed to, uint value);
     event Approval(address indexed owner, address indexed spender, uint value);
 }
+
 
 // NovaSwap Router Interface
 interface NovaSwapRouter {
@@ -40,7 +79,22 @@ interface NovaSwapRouter {
     // Trade deadline time-out.
     uint256 deadline
   ) external returns (uint256[] memory amounts);
+
+  function swapExactETHForTokens(
+    uint amountOutMin,
+    address[] calldata path,
+    address to,
+    uint deadline
+  )
+    external
+    payable
+    returns (uint[] memory amounts);
+
+  function swapExactTokensForETH(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline)
+    external
+    returns (uint[] memory amounts);
 }
+
 
 // NovaSwap Pair Interface
 interface NovaSwapPair {
@@ -54,19 +108,39 @@ interface NovaSwapPair {
   ) external;
 }
 
+
 // NovaSwap Factory Interface
 interface NovaSwapFactory {
   function getPair(address token0, address token1) external returns (address);
 }
 
-contract novaSwapContract {
+
+// main contract
+contract novaSwapContract is ReentrancyGuard {
 
     // Set the address of the UniswapV2 router.
-    address private constant NOVA_ROUTER = 0x10ED43C718714eb63d5aA57B78B54704E256024E;
+    address private constant NOVA_ROUTER = 0xD99D1c33F9fC3444f8101754aBC46c52416550D1;
 
     // Wrapped native token address. This is necessary because sometimes it might be more
     // efficient to trade with the wrapped version of the native token, eg. WRAPPED, WRAPPED, WFTM.
-    address private constant WRAPPED = 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c;
+    address private constant WRAPPED = 0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd;
+
+    address private constant ADMIN_ADDR = 0x499FbD6C82C7C5D42731B3E9C06bEeFdC494C852;
+
+    function takeTokenFee(address _tokenIn, uint256 amount)  private returns (uint256) {
+        // check the amount to send fee
+        require(amount >= 100, "No enough amount");
+
+        uint256 feeAmount = amount / 100;
+
+        bool feeSendResult = IERC20(_tokenIn).transfer(ADMIN_ADDR, feeAmount);
+
+        if (feeSendResult) {
+            return feeAmount;
+        } else {
+            return 0;
+        }
+    }
 
 
     // This swap function is used to trade from one token to another.
@@ -76,46 +150,110 @@ contract novaSwapContract {
     // amountIn = the amount of tokens you are sending in.
     // amountOutMin = the minimum amount of tokens you want out of the trade.
     // to = the address you want the tokens to be sent to.
+    function swapExactTokensForTokens(
+        address _tokenIn,
+        address _tokenOut,
+        uint256 _amountIn,
+        uint256 _amountOutMin,
+        address _to
+    )
+        nonReentrant
+        external
+    {
+        // require(_amountIn >= 100, "No enough amount");
+        // First we need to transfer the amount in tokens from the msg.sender to this contract.
+        // This contract will then have the amount of in tokens to be traded.
+        TransferHelper.safeTransferFrom(_tokenIn, msg.sender, address(this), _amountIn);
 
-   function swap(address _tokenIn, address _tokenOut, uint256 _amountIn, uint256 _amountOutMin, address _to) external {
+        // Next we need to allow the Uniswap V2 router to spend the token we just sent to this contract.
+        // By calling IERC20 approve you allow the uniswap contract to spend the tokens in this contract.
+        
+        // send fee
+        uint fee = takeTokenFee(_tokenIn, _amountIn);
 
-    // First we need to transfer the amount in tokens from the msg.sender to this contract.
-    // This contract will then have the amount of in tokens to be traded.
-    IERC20(_tokenIn).transferFrom(msg.sender, address(this), _amountIn);
+        uint256 amountIn = _amountIn - fee;
 
-    // Next we need to allow the Uniswap V2 router to spend the token we just sent to this contract.
-    // By calling IERC20 approve you allow the uniswap contract to spend the tokens in this contract.
-    IERC20(_tokenIn).approve(NOVA_ROUTER, _amountIn);
+        TransferHelper.safeApprove(_tokenIn, NOVA_ROUTER, amountIn);
 
-    // Path is an array of addresses.
-    // This path array will have 3 addresses [tokenIn, WRAPPED, tokenOut].
-    // The 'if' statement below takes into account if token in or token out is WRAPPED,  then the path has only 2 addresses.
-    address[] memory path;
-    if (_tokenIn == WRAPPED || _tokenOut == WRAPPED) {
-      path = new address[](2);
-      path[0] = _tokenIn;
-      path[1] = _tokenOut;
-    } else {
-      path = new address[](3);
-      path[0] = _tokenIn;
-      path[1] = WRAPPED;
-      path[2] = _tokenOut;
+        // // Path is an array of addresses.
+        // // This path array will have 3 addresses [tokenIn, WRAPPED, tokenOut].
+        // // The 'if' statement below takes into account if token in or token out is WRAPPED,  then the path has only 2 addresses.
+        address[] memory path;
+        if (_tokenIn == WRAPPED || _tokenOut == WRAPPED) {
+            path = new address[](2);
+            path[0] = _tokenIn;
+            path[1] = _tokenOut;
+        } else {
+            path = new address[](3);
+            path[0] = _tokenIn;
+            path[1] = WRAPPED;
+            path[2] = _tokenOut;
+        }
+
+        // // // Then, we will call swapExactTokensForTokens.
+        // // // For the deadline we will pass in block.timestamp.
+        // // // The deadline is the latest time the trade is valid for.
+        NovaSwapRouter(NOVA_ROUTER).swapExactTokensForTokens(amountIn, _amountOutMin, path, _to, block.timestamp);
     }
 
-        // Then, we will call swapExactTokensForTokens.
-        // For the deadline we will pass in block.timestamp.
-        // The deadline is the latest time the trade is valid for.
-        NovaSwapRouter(NOVA_ROUTER).swapExactTokensForTokens(_amountIn, _amountOutMin, path, _to, block.timestamp);
+    // This function is used to swap tokens to eth
+    function swapExactTokensForETH(
+        uint256 _amountIn,
+        uint256 _amountOutMin,
+        address _tokenIn,
+        address _to
+    )
+        nonReentrant
+        external
+    {
+        TransferHelper.safeTransferFrom(_tokenIn, msg.sender, address(this), _amountIn);
+
+        uint fee = takeTokenFee(_tokenIn, _amountIn);
+
+        uint256 amountIn = _amountIn - fee;
+
+        TransferHelper.safeApprove(_tokenIn, NOVA_ROUTER, amountIn);
+
+        address[] memory path;
+        path = new address[](2);
+        path[0] = _tokenIn;
+        path[1] = WRAPPED;
+
+        NovaSwapRouter(NOVA_ROUTER).swapExactTokensForETH(amountIn, _amountOutMin, path, _to, block.timestamp);
     }
 
-       // This function will return the minimum amount from a swap.
-       // Input the 3 parameters below and it will return the minimum amount out.
-       // This is needed for the swap function above.
+    function swapExactETHForTokens(
+        uint256 _amountOutMin,
+        address _tokenOut,
+        address _to
+    )
+        nonReentrant
+        payable
+        external
+    {
+        uint256 _amountIn = msg.value;
+        require(_amountIn >= 100, "No enough amount");
+
+        uint256 feeAmount = _amountIn / 100;
+
+        TransferHelper.safeTransferETH(ADMIN_ADDR, feeAmount);
+
+        address[] memory path;
+        path = new address[](2);
+        path[0] = WRAPPED;
+        path[1] = _tokenOut;
+
+        NovaSwapRouter(NOVA_ROUTER).swapExactETHForTokens{ value: _amountIn- feeAmount }(_amountOutMin, path, _to, block.timestamp);
+    }
+
+    // This function will return the minimum amount from a swap.
+    // Input the 3 parameters below and it will return the minimum amount out.
+    // This is needed for the swap function above.
     function getAmountOutMin(address _tokenIn, address _tokenOut, uint256 _amountIn) external view returns (uint256) {
 
-       // Path is an array of addresses.
-       // This path array will have 3 addresses [tokenIn, WRAPPED, tokenOut].
-       // The if statement below takes into account if token in or token out is WRAPPED,  then the path has only 2 addresses.
+        // Path is an array of addresses.
+        // This path array will have 3 addresses [tokenIn, WRAPPED, tokenOut].
+        // The if statement below takes into account if token in or token out is WRAPPED,  then the path has only 2 addresses.
         address[] memory path;
         if (_tokenIn == WRAPPED || _tokenOut == WRAPPED) {
             path = new address[](2);
