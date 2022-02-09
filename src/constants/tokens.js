f0375cfb7f047f806821d2a5860d56d8588b141f
Paulo Baronceli <paulo@baronceli.com>
2022-02-09 20:33:54 +1300

    ftm token list updated

--- src/constants/tokens.js
+++ src/constants/tokens.js
@@ -1,30 +1,72 @@
 const fantomTokens = [
   {
+    address: "0x8a7dc00bbfcd44344de7bc6e01d82d518032110c",
+    decimals: 18,
+    logoURI: "https://cloudflare-ipfs.com/ipfs/QmX8jtDyZ1tPiz45jaVXtwB63pDiQ2yRQCxyJJuyU8TqLU",
+    name: "Nova Token",
+    symbol: "NBK"
+  }, {
+    address: "0x69d17c151ef62421ec338a0c92ca1c1202a427ec",
+    decimals: 18,
+    logoURI: "https://cloudflare-ipfs.com/ipfs/QmbQ9qCsHv9dqtSxqDvnAjChzirpk8pKB3a22HPumKxk7o",
+    name: "Supernova Token",
+    symbol: "SNT"
+  }, {
     address: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
     wrappedAddress: "0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83",
     decimals: 18,
-    logoURI: "https://tokens.1inch.io/0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee.png",
+    logoURI: "https://novanetwork.io/wp-content/uploads/2021/11/Fantom.png",
     name: "Fantom",
     symbol: "FTM"
   }, {
+    address: "0x38aca5484b8603373acc6961ecd57a6a594510a3",
+    decimals: 18,
+    logoURI: "https://novanetwork.io/wp-content/uploads/2020/11/Bitcoin-BTC-icon.png",
+    name: "Bitcoin",
+    symbol: "WBTC"
+  }, {
+    address: "0x25c130b2624cf12a4ea30143ef50c5d68cefa22f",
+    decimals: 18,
+    logoURI: "https://novanetwork.io/wp-content/uploads/2020/11/Ethereum-ETH-icon.png",
+    name: "Ethereum",
+    symbol: "ETH"
+  }, {
+    address: "0x85dec8c4b2680793661bca91a8f129607571863d",
+    decimals: 18,
+    logoURI: "https://novanetwork.io/wp-content/uploads/2022/02/paintswaptoken_32.webp",
+    name: "PaintSwap",
+    symbol: "BRUSH"
+  }, {
+    address: "0x5cc61a78f164885776aa610fb0fe1257df78e59b",
+    decimals: 18,
+    logoURI: "https://novanetwork.io/wp-content/uploads/2022/02/spiritswap_32.webp",
+    name: "SpiritSwap",
+    symbol: "SPIRIT"
+  }, {
+    address: "0x841fad6eae12c286d1fd18d1d525dffa75c7effe",
+    decimals: 18,
+    logoURI: "https://novanetwork.io/wp-content/uploads/2022/02/spookyswap-boo_32.webp",
+    name: "Spooky Token",
+    symbol: "BOO"
+  }, {
     address: "0x04068DA6C83AFCFA0e13ba15A6696662335D5B75",
     decimals: 6,
-    logoURI: "https://tokens.1inch.io/0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee.png",
+    logoURI: "https://novanetwork.io/wp-content/uploads/2022/02/USDC_32.webp",
     name: "USD Coin",
     symbol: "USDC"
   }, {
+    address: "0x940f41f0ec9ba1a34cf001cc03347ac092f5f6b5",
+    decimals: 6,
+    logoURI: "https://novanetwork.io/wp-content/uploads/2020/11/Tether-USDT-icon.png",
+    name: "Tether USDT",
+    symbol: "USDT"
+  }, {
     address: "0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83",
     decimals: 18,
-    logoURI: "https://tokens.1inch.io/0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee.png",
-    name: "Wrapped Fantom Token",
+    logoURI: "https://novanetwork.io/wp-content/uploads/2022/02/wFtm_32.png",
+    name: "Wrapped Fantom",
     symbol: "WFTM"
-  }, {
-    address: "0xdc301622e621166BD8E82f2cA0A26c13Ad0BE355",
-    decimals: 18,
-    logoURI: "https://tokens.1inch.io/0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee.png",
-    name: "FRAX Token",
-    symbol: "FRAX"
-  }
+  },
 ]
 
 const tokens = {
